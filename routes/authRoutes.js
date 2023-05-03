ada85f32fccf9be7967cbc8b3923d21251087b0d
Kre5imir <33584041+Kre5imir@users.noreply.github.com>
2023-05-03 03:10:46 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 
+router.route('/')
+    .post(authController.login)
+
 router.route('/refresh')
     .get(authController.refresh)
 
